cd70f716c16980749be078f2917e5c698fdee389
Adam <AB-HardWorkBeatsNaturalTalent@users.noreply.github.com>
2022-02-07 01:12:17 +0000

    Guitar has 5 sub-objects

--- main.cpp
+++ main.cpp
@@ -278,11 +278,11 @@ Part 1c assignment:
 /*
 Thing 10) Guitar
 5 properties:
-    1) number of frets (int)
-    2) number of tuners (int)
-    3) string material (std::string)
-    4) type of pickup (std::string)
-    5) tone control level (float) 
+    1) Fingerboard 
+    2) Tuning 
+    3) Strings 
+    4) Pickups 
+    5) Tone Control 
 3 things it can do:
     1) adjust tone
     2) tune a string
